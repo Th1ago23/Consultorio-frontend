96ae023ddb7f726063197f6605f2af836a7113f0
Thiago <thiago.peixots@gmail.com>
2025-04-19 13:12:27 -0300

    Add new config for auth

---
+++ src/components/Layout/AuthLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import LoginRegisterHeader from './LoginRegisterHeader';
+
+const AuthLayout: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <LoginRegisterHeader />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AuthLayout;
