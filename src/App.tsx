c80f1b0f07033020c450392ba99a4ac3839e4b9d
Thiago <thiago.peixots@gmail.com>
2025-04-19 13:15:06 -0300

    update routes for header's

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import HomePage from './pages/HomePage';
 import LoginPage from './components/Auth/LoginPage';
 import RegisterPage from './components/Auth/RegisterPage';
 import Layout from './components/Layout/Layout';
+import AuthLayout from './components/Layout/AuthLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
 
         </Route>
+        <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
       </Routes>
     </BrowserRouter>
   );
