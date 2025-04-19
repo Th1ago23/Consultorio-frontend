8d36f2970d3a183eed72d72667a9f9e5bf6fab02
Thiago <thiago.peixots@gmail.com>
2025-04-19 13:13:15 -0300

    update logic for headear

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,8 +5,8 @@ const Header: React.FC = () => {
 <header className="p-4 transition-colors duration-300 bg-white shadow-md dark:bg-gray-900 dark:text-white">
   <div className="container flex items-center justify-between h-16 mx-auto">
   <a href="/" aria-label="Back to homepage" className="flex items-center -mt-2 space-x-2 transition-transform duration-300 hover:scale-105">
-  <img src="/Dra. Tatiane.png" alt="Logo Dra. Tatiane" style={{ width: '150px', height: 'auto' }} />
-</a>
+    <img src="/Dra. Tatiane.png" alt="Logo Dra. Tatiane" style={{ width: '150px', height: 'auto' }} />
+  </a>
 
     {/* Navigation */}
     <ul className="hidden space-x-6 lg:flex animate-fade-in">
